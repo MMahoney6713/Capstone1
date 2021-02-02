f699dfeca35155fca21a5400154a5734472c88ac
mmahoney6713 <mrm257@cornell.edu>
2021-02-01 22:21:35 -0500

    added some lo-fi images for showing how the app is supposed to work

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ app.config['SECRET_KEY'] = 'f3cfe9ed8fae309f02079dbf'
 @app.route('/')
 def show_home():
     return render_template('homepage.html')
+
+
+@app.route('/login')
+def login_user():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register_user():
+    return render_template('register.html')
